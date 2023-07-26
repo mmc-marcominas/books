ef8e743cc03c1fc43441979c69d7676791435988
Marco Minas <mmc.marcominas@gmail.com>
2023-07-26 20:29:25 -0300

    docs: add docstring on schemas

--- src/lib/handlers/book/update.js
+++ src/lib/handlers/book/update.js
@@ -1,11 +1,18 @@
 'use strict'
 
+/** 
+* Update a book.
+* @summary Update a book according id sent on HTTP params and value from request body.
+* @param {Request} request - HTTP request object with id in params and book in body.
+* @param {Response} reply - HTTP response object.
+* @return {Object} Return object id and success messaga if update occours otherwise throw error.
+*/
 module.exports = async function updateBook(request, reply) {
   const { id } = request.params
 
   const values = this.database.parseBook(request.body)
   const result = await this.database.update(id, values)
-  
+
   if (!result.updated) {
     const error = new Error('Book not found: ' + request.params.id)
     error.status = 404
